58134cca90f2e5c5acd3ddb18292172f4eb53744
surajvns1571999 <surajvns1571999@gmail.com>
2025-09-21 17:24:36 +0530

    creating normal queue

---
+++ PepCoding/DSA_LEVEL_1/src/com/stackQueueLinkedList/OneThreeOneToTwoOneThree/PEP143_MinimumStack2.java
@@ -0,0 +1,78 @@
+package com.stackQueueLinkedList.OneThreeOneToTwoOneThree;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class PEP143_MinimumStack2 {
+	
+
+	public static void main(String[] args) {
+		
+		Stack<Integer> allData = new Stack<Integer>();
+		int minData =0;
+		Scanner sc=new Scanner(System.in);
+		Integer min=Integer.MAX_VALUE;
+		int a;
+		
+		while (true) {
+			System.out.println(
+					"PLZ inter value \n 1 : PUSH \t 2 : POP \t 3 : TOP \t 4 : SIZE \t 5 : DISPLAY \t 6 : MIN VALUE \t 0 : EXIT");
+
+			int index = Integer.parseInt(sc.nextLine());
+
+			switch (index) {
+
+			case 0:
+				System.out.println("We will meet again, Good By !!!");
+				System.exit(0);
+
+			case 1:
+				System.out.print("Enter the value: ");
+				a = Integer.parseInt(sc.nextLine());
+				if(allData.isEmpty()) {allData.add(a); minData=a;}
+				else if(a>minData) allData.add(a);
+				else {
+					allData.add(2*a-minData); 
+					minData=a;
+				}
+				break;
+
+			case 2:
+				if(allData.isEmpty()) System.out.println("Stack is Empty !!!\n");
+				else {
+					a=allData.peek();
+					if(a<minData) {
+					minData = 2*minData-a;
+					}
+					allData.pop();
+				}
+				break;
+
+			case 3:
+				if(allData.isEmpty()) System.out.println("Stack is Empty !!!\n");
+				else System.out.println("Stack Top Value : "+ allData.peek()+"\n");
+				break;
+
+			case 4:
+				System.out.println("Stack size : "+allData.size());
+				break;
+
+			case 5:
+				System.out.println("Stack Element : "+allData);
+				break;
+			
+			case 6:
+				if(allData.isEmpty()) System.out.println("Stack is Empty !!!\n");
+				else System.out.println("Minimum element is : "+minData);
+				break;
+				
+			default:
+				System.out.println("Invalid value ! \n PLZ Inter value between 0 to 5 only");
+
+			}
+
+		}
+
+	}
+
+}
---
+++ PepCoding/DSA_LEVEL_1/src/com/stackQueueLinkedList/OneThreeOneToTwoOneThree/PEP146_BuildNormalQueue.java
@@ -0,0 +1,111 @@
+package com.stackQueueLinkedList.OneThreeOneToTwoOneThree;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class PEP146_BuildNormalQueue {
+	static Scanner sc = new Scanner(System.in);
+	static int[] allData;
+	static int sizeq;
+
+	static void initQueue() {
+		System.out.println("Enter the size of queue: ");
+		sizeq = Integer.parseInt(sc.nextLine());
+		allData = new int[sizeq];
+	}
+
+	static int getSize(int front, int rear) {
+		if (front == -1)
+			return 0;
+		else if (rear >= front)
+			return rear - front + 1;
+		else
+			return sizeq - front + rear + 1;
+	}
+
+	public static void main(String[] args) {
+		initQueue();
+		int front = -1, rear = -1;
+
+		while (true) {
+			System.out.println(
+					"PLZ select the operation \n 1 : SIZE \t 2 : DISPLAY \t 3 : ADD \t 4 : REMOVE \t 5 : PEEK \t 0 : EXIT");
+
+			int index = Integer.parseInt(sc.nextLine());
+
+			switch (index) {
+
+			case 0:
+				System.out.println("We will meet again, Good By !!!");
+				System.exit(0);
+
+			case 1:
+				System.out.println("Size of queue: " + getSize(front, rear) + "\n");
+				break;
+
+			case 2:
+				if (getSize(front, rear) != 0) {
+					System.out.print("Element of queue: ");
+					if(rear>=front) {
+					for (int i = front; i <=rear; i++) {
+						System.out.print(allData[i]+"  ");
+					}}else {
+						int x=front;
+						do {
+							System.out.print(allData[x]+"  ");
+							x=(x+1)%sizeq;
+						}while(x!=rear);
+					}
+					System.out.println();
+				} else {
+					System.out.println("Queue is empty!!!");
+				}
+				break;
+
+			case 3:
+				System.out.println("Enter the element: ");
+				int a = Integer.parseInt(sc.nextLine());
+				if (front == -1) {
+					rear = 0;
+					front = 0;
+					allData[rear] = a;
+				} else if (getSize(front, rear) != sizeq) {
+					rear = (rear + 1) % sizeq;
+					allData[rear] = a;
+				} else {
+					System.out.println("Queue is full !!\n");
+				}
+				break;
+
+			case 4:
+
+				if (getSize(front, rear) == 0) {
+					System.out.println("Queue is empty!!!");
+				} else {
+					System.out.println("Remove element is : " + allData[front]);
+					if (front == rear) {
+						front = -1;
+						rear = -1;
+					} else
+						front = (front + 1) % sizeq;
+				}
+				break;
+
+			case 5:
+				if (getSize(front, rear) == 0) {
+					System.out.println("Queue is empty!!!");
+				} else {
+					System.out.println(allData[rear]);
+				}
+				break;
+
+			default:
+				System.out.println("Invalid value ! \n PLZ Inter value between 0 to 5 only");
+
+			}
+
+		}
+
+	}
+
+}
