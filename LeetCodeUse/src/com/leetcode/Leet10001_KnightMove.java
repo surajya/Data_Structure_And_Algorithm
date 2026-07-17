3fd62b6a418acca5871dfe01788e8e7ce55539ce
surajvns1571999 <surajvns1571999@gmail.com>
2026-07-17 21:15:30 +0530

    dynamic programming

---
+++ LeetCodeUse/src/com/leetcode/Leet10001_KnightMove.java
@@ -0,0 +1,80 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+class CheckPoint{
+	int i;
+	int j;
+
+	CheckPoint(int a, int b){
+		this.i = a;
+		this.j = b;
+	}
+
+	public int getI(){
+		return this.i;
+	}
+
+	public int getJ() {
+		return j;
+	}
+}
+
+public class Leet_KnightMove {
+
+	public static void main(String[] args) {
+		int[][] arr = new int[4][4];
+		List<CheckPoint> knightPointList = new ArrayList<>();
+		knightPointList.addAll(Arrays.asList(
+				(new CheckPoint(1,2)),
+				(new CheckPoint(2,1)),
+				(new CheckPoint(2,-1)),
+				(new CheckPoint(1,-2)),
+				(new CheckPoint(-1,-2)),
+				(new CheckPoint(-2,-1)),
+				(new CheckPoint(-2,1)),
+				(new CheckPoint(-1,2))));
+		System.out.println("all path is :"+ findKnightMove(arr, 0, 0, knightPointList, "00-", 1));
+//		List<String> listWay = findWayToSit(arr, 0, new int[arr.length], new int[arr[0].length], "");
+//		List<List<String>> ans = new ArrayList<>();
+//		for(String str : listWay){
+//			String[] qPoint = str.split("-");
+//			List<String> tempPath = new ArrayList<>();
+//			for(String point : qPoint){
+//				int x = point.charAt(0)-'0';
+//				int y = point.charAt(1)-'0';
+//				StringBuilder path = new StringBuilder();
+//				for(int i=0; i<arr.length; i++){
+//					if(i==y) path.append("Q");
+//					else path.append(".");
+//				}
+//				tempPath.add(path.toString());
+//			}
+//			ans.add(tempPath);
+//		}
+//		System.out.println(ans);
+	}
+
+	private static List<String> findKnightMove(int[][] arr, int i, int j, List<CheckPoint> knightPointList, String way, int totalvisit) {
+		if(totalvisit > 15) return Collections.singletonList(way);
+
+		arr[i][j] = 1;
+		ArrayList<String> ways = new ArrayList<>();
+		for(CheckPoint point : knightPointList){
+			int x = i+ point.getI();
+			int y = j+ point.getJ();
+			if(checkSafe(arr, x, y) && arr[x][y] != 1){
+				ways.addAll(findKnightMove(arr, x, y, knightPointList, way+x+y+"-", totalvisit+1));
+			}
+		}
+		arr[i][j] = 0;
+		return ways;
+	}
+
+	private static boolean checkSafe(int[][] arr, int i, int j) {
+        return i >= 0 && i< arr.length && j >= 0 && j < arr[0].length;
+	}
+}
---
+++ PepCoding/DSA_LEVEL_1/src/com/dynamicProgramming/PEP0432_TiltOfBinaryTree.java
@@ -0,0 +1,32 @@
+package com.dynamicProgramming;
+
+public class PEP0432_TiltOfBinaryTree {
+
+    public static void main(String[] args){
+        int[] nodeArray = {21,7,14,1,1,2,2,3,3};
+        TreeNode rootNode = TreeNode.createTree(nodeArray, 0);
+        displayBT(rootNode);
+        System.out.println();
+        System.out.print("Tilt value of BT : "+ findTiltBinaryTree(rootNode));
+        System.out.println();
+    }
+
+    private static void displayBT(TreeNode rootNode) {
+        if(rootNode == null) return;
+        displayBT(rootNode.left);
+        System.out.print(rootNode.val + " ");
+        displayBT(rootNode.right);
+    }
+
+    public static int findTiltBinaryTree(TreeNode root){
+        if(root == null) return 0;
+
+        int leftTilt = findTiltBinaryTree(root.left);
+        int rightTilt = findTiltBinaryTree(root.right);
+        int rootTilt = Math.abs((root.left!=null ? root.left.val : 0) - (root.right!=null ? root.right.val : 0));
+        root.val = root.val + (root.left!=null ? root.left.val : 0) + (root.right!=null ? root.right.val : 0);
+
+        return (leftTilt + rightTilt + rootTilt);
+    }
+
+}
